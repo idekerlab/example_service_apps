6d3ebf3a242c0864c66ec956cef1f3ba11fee013
Chris Churas <churas.camera@gmail.com>
2024-12-23 15:22:05 -0800

    enabled cors

--- python-flask/examplecywebserviceapp/__main__.py
+++ python-flask/examplecywebserviceapp/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import connexion
+from flask_cors import CORS
 
 from examplecywebserviceapp import encoder
 
@@ -11,6 +12,18 @@ def main():
     app.add_api('openapi.yaml',
                 arguments={'title': 'Example Cytoscape Service-App API'},
                 pythonic_params=True)
+    CORS(app.app, resources={r"/*": {"origins": "*",
+                                     "methods": ["HEAD", "DELETE",
+                                                 "GET", "OPTIONS",
+                                                 "POST", "PUT"]}})
+
+    @app.app.after_request
+    def add_cors_headers(response):
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, PUT, POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return response
+
 
     app.run(port=8080)
 
